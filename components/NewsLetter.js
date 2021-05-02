4de845ab6ea002eb82e01fbeddf5e3b71364acaa
albertcarrete <albertcarrete@gmail.com>
2021-05-02 14:43:54 -0700

    Update copywriting and layout

--- components/NewsLetter.js
+++ components/NewsLetter.js
@@ -1,18 +1,20 @@
 import NewsLetterForm from "./Forms/NewsLetterForm";
 export default function NewsLetter() {
   return (
-    <div className="bg-gray-200 py-20">
+    <div className="bg-gray-200 py-10 md:py-20">
       <div className="bumper lg:flex items-center">
-        <div className="lg:w-1/2 mb-8 lg:mb-0">
-          <div className="pr-20">
-            <p className="text-3xl mb-2">Get 3D Printing News in your inbox</p>
-            <p className="text-lg">
-              Subscribe for bi-weekly updates on the latest happening in 3D
-              printing homes, furniture, and more!
+        <div className="w-full lg:w-1/2 mb-8 lg:mb-0">
+          <div className="lg:pr-20">
+            <p className="text-2xl md:text-3xl mb-2">
+              Subscribe for 3D Printing News
+            </p>
+            <p className="text-sm md:text-lg">
+              Get bi-weekly updates on the latest happening in 3D printing
+              homes, furniture, and more!
             </p>
           </div>
         </div>
-        <div className="md:w-1/2">
+        <div className="lg:w-1/2">
           <NewsLetterForm />
         </div>
       </div>
