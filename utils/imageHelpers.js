6679e828363fa6801fffaa6da015a37f4bfc96bc
albertcarrete <albertcarrete@gmail.com>
2021-05-02 14:43:02 -0700

    Add thumbnail sizing

--- utils/imageHelpers.js
+++ utils/imageHelpers.js
@@ -3,3 +3,6 @@ export const img = (image, size) => {
     size ? size : 1500
   }`;
 };
+export const thumbnail_sd = (image, size) => {
+  return `${process.env.NEXT_PUBLIC_API_IMAGE_CDN}/${image}?fit=crop&w=720&h=576`;
+};
