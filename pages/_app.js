892314320045fce8124fd53dbe2c0aed0e4980c2
albertcarrete <albertcarrete@gmail.com>
2021-05-11 14:36:24 -0700

    Add Google Tag Manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,23 @@
 import "../styles/globals.scss";
 import Layout from "../components/Layout";
 import { DefaultSeo } from "next-seo";
+import GoogleTagManager from "../components/GoogleTagManager";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <DefaultSeo
-        titleTemplate={`%s | Think3DP`}
-        defaultTitle="Think3DP | 3D Printing News"
-        openGraph={{
-          locale: "en_US",
-          site_name: "Think3DP",
-        }}
-      />
-      <Component {...pageProps} />
-    </Layout>
+    <GoogleTagManager>
+      <Layout>
+        <DefaultSeo
+          titleTemplate={`%s | Think3DP`}
+          defaultTitle="Think3DP | 3D Printing News"
+          openGraph={{
+            locale: "en_US",
+            site_name: "Think3DP",
+          }}
+        />
+        <Component {...pageProps} />
+      </Layout>
+    </GoogleTagManager>
   );
 }
 
