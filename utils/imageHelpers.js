c885882f56a1d87c2cb9eae8950a4ab9a79d1fe3
albertcarrete <albertcarrete@gmail.com>
2021-06-02 20:10:32 -0700

    Add opengraph image helper

--- utils/imageHelpers.js
+++ utils/imageHelpers.js
@@ -3,6 +3,9 @@ export const img = (image, size) => {
     size ? size : 1500
   }`;
 };
+export const openGraphImage = (image) => {
+  return `${process.env.NEXT_PUBLIC_API_IMAGE_CDN}/${image}?fit=crop&w=1200&h=627`;
+};
 export const thumbnail_sd = (image, size) => {
   return `${process.env.NEXT_PUBLIC_API_IMAGE_CDN}/${image}?fit=crop&w=720&h=576`;
 };
